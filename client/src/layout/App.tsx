559f790706439a8a1f42328a39fe293e65bcbd81
Michael Gainey <michael@Michaels-MacBook-Pro-2.local>
2022-06-06 20:57:26 -1000

    changed file structure

---
+++ client/src/layout/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/App.css';
+import SecondComponent from '../components/SecondComponent';
+import StarterComponent from '../components/StarterComponent';
+
+function App() {
+
+  return (
+    <div className="App">
+      <StarterComponent/>
+      <SecondComponent/>
+    </div>
+  );
+}
+
+export default App;
