0e50b4213d6cf1d17c31505ef9122f6a1f8cced8
Michael Gainey <mgainey@hawaii.edu>
2022-06-16 05:15:49 -1000

    made a dummy typescript component as a template

--- client/src/layout/App.tsx
+++ client/src/layout/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/App.css';
 import SecondComponent from '../components/SecondComponent';
 import StarterComponent from '../components/StarterComponent';
+import Person from '../components/person/Person';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <StarterComponent/>
       <SecondComponent/>
+      <Person name='Billy' age={15} likes='idk'/>
     </div>
   );
 }
